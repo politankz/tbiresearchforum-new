ba124531b9c99b6405477b40eafd505255130d10
politankz <dmhorton@gmail.com>
2013-05-01 22:46:37 -0700

    First (pre release) commit

---
+++ TBIResearchForum/new-www/tab_bar_test.php
@@ -0,0 +1,25 @@
+<div id="tab-bar" data-role="tabbar" data-iconpos="top" data-theme="a"
+    class="navigation-objects">
+    <ul>
+        <li>
+            <a href="#page1" data-transition="none" data-theme="" data-icon="info">
+                Info
+            </a>
+        </li>
+        <li>
+            <a href="#page6" data-transition="none" data-theme="" data-icon="grid">
+                Posters
+            </a>
+        </li>
+        <li>
+            <a href="#page7" data-transition="none" data-theme="" data-icon="alert">
+                Speakers
+            </a>
+        </li>
+        <li>
+            <a href="#page8" data-transition="none" data-theme="" data-icon="check">
+                Survey
+            </a>
+        </li>
+    </ul>
+</div>
